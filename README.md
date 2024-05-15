fbd9d58a0358dc834fecf22254a0a095981c5ef9
JParker3110 <156866178+JParker3110@users.noreply.github.com>
2024-05-15 09:26:18 -0400

    updated readme js

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Capstone2
-[Live Site]
-An app that displays your favorite recipes
+Author: Janiece Parker
+
+[Live Site] http://127.0.0.1:5500/index.html?
+An app that displays your favorite recipes!
+
+Feature Tasks
+[x] Structure HTML
+[x] Styling the CSS page using color, fonts, images and text
+[] Connect HTML with JavaScript
+[] Create code to display recipes
--- js/app.js
+++ js/app.js
@@ -4,12 +4,16 @@ function getRecipe(event) {
   // console.log("works")
   const cookBook = event.target["recipe-input"].value;
   const url = Base_URL + `/1/search.php?s=${cookBook}`;
-// console.log(url)
+  // console.log(url)
   fetch(url)
     .then((res) => res.json())
-    .then((data) => {console.log(data)});
-    for (let i = 0; i < data.meals.length; i++){
-        console.log(data.meals[i].strMeal)
-      }
+    .then((data) => {
+      console.log(data);
+    });
 
+  let searchField = document.getElementById("search-area");
+  let searchArea = searchField.value;
+  for (let i = 0; i < data.meals.length; i++) {
+    data.meals[i].strMeal;
+  }
 }
